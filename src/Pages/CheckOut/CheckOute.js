b3bff20fcf087ddb16aaab9eca0d4687101b9d11
jabedit <sylhet.bd321@gmail.com>
2022-10-27 23:54:07 +0600

    final final final final

--- src/Pages/CheckOut/CheckOute.js
+++ src/Pages/CheckOut/CheckOute.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../Context/AuthProvider';
 
 const CheckOute = () => {
     const checkOutInfo = useLoaderData()
+    const {user} = useContext(AuthContext)
+    console.log(user)
     return (
-        <div>
-            This is Check oute page {checkOutInfo.title}
+        <div className='course-card '>
+            <h3> {checkOutInfo.title} </h3>
+            { user? <h3> Thank you for joining this course { user.displayName } </h3> : '' }
+            
         </div>
     );
 };
